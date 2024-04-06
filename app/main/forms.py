965fc456aa9455b8bde6e15191b7fc718612c067
scottsaenz <scottrsaenz@gmail.com>
2024-04-06 11:47:52 -0400

    added search

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,6 +5,7 @@ import sqlalchemy as sa
 from flask_babel import _, lazy_gettext as _l
 from app import db
 from app.models import User
+from flask import request
 
 
 class EditProfileForm(FlaskForm):
@@ -33,3 +34,13 @@ class PostForm(FlaskForm):
     post = TextAreaField(_l('Say something'), validators=[
         DataRequired(), Length(min=1, max=140)])
     submit = SubmitField(_l('Submit'))
+
+class SearchForm(FlaskForm):
+    q = StringField(_l('Search'), validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'meta' not in kwargs:
+            kwargs['meta'] = {'csrf': False}
+        super(SearchForm, self).__init__(*args, **kwargs)
